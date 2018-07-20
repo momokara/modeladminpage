122b7fff2a0dbf1563da47d75787da6789c4c6ad
momokara <1125076@qq.com>
2018-07-20 15:45:27 +0800

    1

--- src/app/member/memberbase/memberbase.component.ts
+++ src/app/member/memberbase/memberbase.component.ts
@@ -10,6 +10,13 @@ export class MemberbaseComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    indexedDB.open('testdb1', 1);
+    // let db = openDatabase("noteDB", "1.0", "noteDB", 1024 * 1024 * 10, function (result) {
+    //   console.log(result);
+    //   // 只有创建数据库成功 才会调用这个回调
+    //   db = result;
+    // });
   }
 
 }
