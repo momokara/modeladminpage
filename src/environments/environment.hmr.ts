ca6f272cf86857d8a93824308929e1c69f27fe9f
momokara <1125076@qq.com>
2018-08-19 16:48:52 +0800

    仔细想想好像不需要未HMR 专门写个环境文件

--- src/environments/environment.hmr.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  // 是否生产模式
-  production: true,
-  // 是否是本地测试 为true 的时候 会以base 访问本地json 方便调样式
-  istest: true,
-  hmr: true,
-  // api 基本路径
-  baseurl: { baseherf: 'base/index/index/' }
-};
