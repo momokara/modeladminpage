0d5c9b04d06ed85d5309835cdd307396e5b93bfb
momokara <1125076@qq.com>
2018-08-19 16:44:51 +0800

    加入一个 HMR 范例

--- src/environments/environment.hmr.ts
+++ src/environments/environment.hmr.ts
@@ -2,8 +2,8 @@ export const environment = {
   // 是否生产模式
   production: true,
   // 是否是本地测试 为true 的时候 会以base 访问本地json 方便调样式
-  istest: false,
-  hmr: false,
+  istest: true,
+  hmr: true,
   // api 基本路径
-  baseurl: { baseherf: '/index/index/' }
+  baseurl: { baseherf: 'base/index/index/' }
 };
